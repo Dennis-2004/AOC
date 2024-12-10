5a55542b9f9791df701224fdb2c7e48db7bf0832
Dennis-2004 <117187297+Dennis-2004@users.noreply.github.com>
2024-12-10 13:04:22 +0100

    day10

---
+++ AOC/Advent of Code 2024/day10.py
@@ -0,0 +1,28 @@
+def part1(map, part2=False):
+    total = 0
+    starts = sum([[(x, y) for x in range(len(map[0])) if map[y][x] == 0] for y in range(len(map))], [])
+
+    for start in starts:
+        ends = 0
+        stack = []
+        visited = []
+        stack.append(start)
+        while len(stack):
+            x, y = stack.pop()
+            if not part2:
+                visited.append((x, y))
+            for dx, dy in [(-1, 0), (0,1), (1, 0), (0, -1)]:
+                if 0 <= y+dy < len(map) and 0 <= x+dx < len(map[0]):
+                    if map[y + dy][x + dx] - map[y][x] == 1 and (x + dx, y + dy) not in visited:
+                        stack.append((x + dx, y + dy))
+                        if map[y + dy][x + dx] == 9:
+                            ends += 1
+        total += ends
+    return total
+
+map = open("day10.txt").readlines()
+for x in range(len(map)):
+    map[x] = [int(i) for i in map[x].split()[0]]
+
+print(part1(map))
+print(part1(map, True))
